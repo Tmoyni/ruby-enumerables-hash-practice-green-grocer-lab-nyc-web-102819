3daceeda4082503c2864731e0b8a302b136a667f
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-05 23:36:32 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -40,5 +40,8 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  # code here
+  hash_cart = consolidate_cart(array)
+  applied_coupons = apply_coupons(hash_cart, coupons)
+  applied_discount = apply_clearance(applied_coupons)
+  
 end
