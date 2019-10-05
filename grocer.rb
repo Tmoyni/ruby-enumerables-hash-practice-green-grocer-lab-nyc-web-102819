7d08984541a47e152a911300a3deb2753b3a4130
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-05 23:59:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -23,7 +23,7 @@ def apply_coupons(cart, coupons)
       if !cart["#{item} W/COUPON"] && cart[item][:count] >= coupon[:num]
          cart["#{item} W/COUPON"] = {count: coupon[:num], price: coupon[:cost]/coupon[:num], clearance: cart[item][:clearance]}
          cart[item][:count] -= coupon[:num]
-      elsif cart[item][:count] >= coupon[:num] && cart.has_key?("#{item} W/COUPON")
+      elsif cart["#{item} W/COUPON"] && cart[item][:count] >= coupon[:num]
         cart["#{item} W/COUPON"][:count] += coupon[:num]
         cart[item][:count] -= coupon[:num]
       end
