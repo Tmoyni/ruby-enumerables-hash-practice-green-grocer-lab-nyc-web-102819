6959c13b0aa7a548f6f83b561a4f81db3e021cef
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-05 22:18:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,7 +19,9 @@ end
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     item = coupon[:item]
-    
+    if cart[item] && coupon[:item][:count] >= !coupon[:num] && cart["#{item}W/COUPON"]
+    else
+    end
   end
 end
 
