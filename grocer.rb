489e10532a561155ebfd37f7b07ee0ca32ac1a8f
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-06 00:07:12 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -27,8 +27,8 @@ def apply_coupons(cart, coupons)
            cart[item][:count] -= coupon[:num]
         else
           cart[coupon_item][:count] += coupon[:num]
-      end
-      cart[item][:count] -= coupon[:num]
+        end
+        cart[item][:count] -= coupon[:num]
      end
   end
 end
