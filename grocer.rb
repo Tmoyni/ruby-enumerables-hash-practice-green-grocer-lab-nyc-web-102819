b45c4d66444169aacb3536888ef4b27a2c454797
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-06 00:02:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -21,11 +21,11 @@ def apply_coupons(cart, coupons)
     item = coupon[:item]
     coupon_item = "#{item} W/COUPON"
     if cart.has_key?(item)
-      if !cart["#{item} W/COUPON"] && cart[item][:count] >= coupon[:num]
-         cart["#{item} W/COUPON"] = {count: coupon[:num], price: coupon[:cost]/coupon[:num], clearance: cart[item][:clearance]}
+      if !cart[coupon_item] && cart[item][:count] >= coupon[:num]
+         cart[coupon_item] = {count: coupon[:num], price: coupon[:cost]/coupon[:num], clearance: cart[item][:clearance]}
          cart[item][:count] -= coupon[:num]
-      elsif cart["#{item} W/COUPON"] && cart[item][:count] >= coupon[:num]
-        cart["#{item} W/COUPON"][:count] += coupon[:num]
+      elsif cart[coupon_item] && cart[item][:count] >= coupon[:num]
+        cart[coupon_item][:count] += coupon[:num]
         cart[item][:count] -= coupon[:num]
       end
      end
